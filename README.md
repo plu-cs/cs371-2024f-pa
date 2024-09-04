ae2a9c0c02def3a6e4811a61a58f8ce30ae13d85
DW <924004+daw42@users.noreply.github.com>
2024-09-04 11:34:40 -0700

    Framework for PA 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSCI 371 - Programming Assignments
+
+Fall 2024
+
+Each programming assignment is located within a separate directory.
---
+++ pa01/AclAnalyzer.java
@@ -0,0 +1,8 @@
+public class AclAnalyzer {
+
+    public static void main(String[] args) {
+        
+        // TODO: Implement your solution here.  Use the first command line argument as the input file name.
+
+    }
+}
